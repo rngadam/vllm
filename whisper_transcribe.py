956a95bc871d5b0458216aa1674b8226a4356514
Ricky Ng-Adam <rngadam@gmail.com>
2025-02-20 09:31:32 +0000

    updated transcriptions pipeline

--- whisper_transcribe.py
+++ whisper_transcribe.py
@@ -1,6 +1,26 @@
+#!/usr/bin/env python
 import whisper
+import sys
+import json
+import os.path
+import mimetypes
 
-model = whisper.load_model("tiny")
-result = model.transcribe("/media/Videos/PXL_20240605_150920501.mp4")
+model = whisper.load_model("base")
 
-print(result["text"])
+for filename in sys.argv[1:]:
+    output_filename = f"{filename}.whisper.json"
+    if os.path.isfile(output_filename):
+        continue
+    mimetype, encoding = mimetypes.guess_type(filename)
+    if not mimetype or not mimetype.startswith('video'):
+        continue
+    print(f"{filename}: {mimetype}")
+    try:
+        result = model.transcribe(filename)
+    except:
+        print(f"processing failed for {filename}")
+        continue
+    print(result['text'])
+    with open(output_filename, "w+") as f:
+        print(f"{filename} -> {output_filename}")
+        json.dump(result, f, indent=4)
